acdd5652fc7b2e6461384b8480d13fc9d46bca8c
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-05 22:59:18 +0900

    ActiveHashの各モデルファイルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :Ship_charge
+  belongs_to :Ship_from
+  belongs_to :ship_days
+
   has_one_attached :image
   belongs_to :user
 end
