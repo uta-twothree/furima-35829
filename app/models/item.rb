16059702a342bded4b9f96f25edaaaaf2b4a1af4
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-06 11:22:14 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,22 @@ class Item < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :image
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition
+      validates :ship_charge_id
+      validates :ship_from_id
+      validates :ship_days_id
+    end
+  end
+
+  with_options presence: true, numericality: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/ } do
+    validates :price
+  end
+
 end
