1bd76aa1bb479988dae79d4aabb0a069b3f85fcb
uta-twothree <yuta_ftm0718@icloud.com>
2021-06-09 12:25:02 +0900

    アソシエーションの定義

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :ship_from_id
+
+  belongs_to :user
+  belongs_to :item
+  has_one :address
 end
